d8e1e29284b08b5bf179db568a2493757d30143f
Sarah Walling-Bell <sarah.wallingbell@alleninstitute.org>
2023-11-10 14:48:38 -0800

    add: average layers

---
+++ morph_utils/templates.py
@@ -0,0 +1,13 @@
+
+#mouse average coritcal layers
+mouse_layer_depths = {"1":0,"2\/3":115.1112491335,"4":333.4658190171,"5":453.6227158132,"6a":687.6482650269,"6b":883.1308910545,"wm":922.5861720311}
+mouse_layer_colors = {"1":"darkgrey", "2\/3":"lightgrey","4":"lightgrey","5":"lightgrey","6a":"lightgrey","6b":"lightgrey","wm":"darkgrey"}
+mouse_layer_labels = {"L1": 57.55562456675,"L2\/3": 224.2885340753,"L4": 393.54426741515,"L5": 570.63549042005,"L6a": 785.3895780407,"L6b": 902.8585315427999}
+
+#human average cortical layers
+human_layer_depths = {"1":0,"2":247.9338740595,"3":410.0775480852,"4":1322.3048788711,"5":1557.8034331092,"6":2189.3775739155,"wm":3077.3662871205}
+human_layer_colors = {"1":"darkgrey", "2":"lightgrey","3":"lightgrey","4":"lightgrey","5":"lightgrey","6":"lightgrey","wm":"darkgrey"}
+human_layer_labels = {"L1": 123.96693702975,"L2": 329.00571107235,"L3": 866.1912134781501,"L4": 1440.0541559901499,"L5": 1873.5905035123499,"L6": 2633.371930518}
+
+#nhp average cortical layers
+#TODO add when we have them 
