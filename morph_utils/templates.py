1be01dfb231802ec92f4e238f0f277dd24139bfa
Sarah Walling-Bell <sarah.wallingbell@alleninstitute.org>
2023-11-13 13:03:19 -0800

    edit: load avg layers from json

--- morph_utils/templates.py
+++ morph_utils/templates.py
@@ -1,13 +1,34 @@
+import json
+import numpy as np
 
-#mouse average coritcal layers
-mouse_layer_depths = {"1":0,"2\/3":115.1112491335,"4":333.4658190171,"5":453.6227158132,"6a":687.6482650269,"6b":883.1308910545,"wm":922.5861720311}
-mouse_layer_colors = {"1":"darkgrey", "2\/3":"lightgrey","4":"lightgrey","5":"lightgrey","6a":"lightgrey","6b":"lightgrey","wm":"darkgrey"}
-mouse_layer_labels = {"L1": 57.55562456675,"L2\/3": 224.2885340753,"L4": 393.54426741515,"L5": 570.63549042005,"L6a": 785.3895780407,"L6b": 902.8585315427999}
+def load_layer_template(species):
+    """ Load the average cortical layer depth JSON file for a species.
 
-#human average cortical layers
-human_layer_depths = {"1":0,"2":247.9338740595,"3":410.0775480852,"4":1322.3048788711,"5":1557.8034331092,"6":2189.3775739155,"wm":3077.3662871205}
-human_layer_colors = {"1":"darkgrey", "2":"lightgrey","3":"lightgrey","4":"lightgrey","5":"lightgrey","6":"lightgrey","wm":"darkgrey"}
-human_layer_labels = {"L1": 123.96693702975,"L2": 329.00571107235,"L3": 866.1912134781501,"L4": 1440.0541559901499,"L5": 1873.5905035123499,"L6": 2633.371930518}
+    Keys are strings representing cortical layers (e.g.'2/3','4'...)
+    Values represent the cortical depth for the top (pia side) of a given layer
 
-#nhp average cortical layers
-#TODO add when we have them 
+    TODO add NHP average layers when generated. 
+
+    :param species: species of average layers to return. Options: 'mouse' or 'human' 
+    :return: layers, dictionary of distances to the pia (in microns) from the upper side of each layer.
+    :return: labels, dictionary of layer names (e.g. 'L1', 'L2/3') and midpoints 
+    :return: colors, dictionary of layer line colors 
+    """
+    
+    #average layer depths 
+    depth_file = "./data/layer_templates/{}_average_layer_depths.json".format(species)
+    with open(depth_file, "r") as fn: 
+        layers = json.load(fn)
+
+    #layer line colors 
+    layer_keys = list(layers.keys())
+    layer_colors = ['darkgrey' if l in ['1', 'wm'] else 'lightgrey' for l in layer_keys]
+    colors = dict(zip(layer_keys, layer_colors))
+
+    #layer labels and midpoints 
+    layer_depths = np.array(list(layers.values()))
+    layer_midpoints = list((layer_depths[1:] + layer_depths[:-1]) / 2)
+    layer_names = ['L' + l for l in layer_keys[:-1]]
+    labels = dict(zip(layer_names, layer_midpoints))
+
+    return layers, labels, colors
