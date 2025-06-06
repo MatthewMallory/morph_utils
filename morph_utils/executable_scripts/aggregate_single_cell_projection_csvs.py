34cc47550ed3433da3c24b09b2c2fe518c5f35a0
Matt Mallory <matt.mallory@alleninstitute.org>
2025-06-05 23:49:38 -0700

    add: roll up to proj mat agg

--- morph_utils/executable_scripts/aggregate_single_cell_projection_csvs.py
+++ morph_utils/executable_scripts/aggregate_single_cell_projection_csvs.py
@@ -2,7 +2,10 @@ import os
 from tqdm import tqdm
 import pandas as pd
 import argschema as ags
+import numpy as np
 from morph_utils.ccf import projection_matrix_for_swc
+from morph_utils.ccf import STRUCTURE_DESCENDANTS_ACRONYM
+
 
 class IO_Schema(ags.ArgSchema):
     output_directory = ags.fields.OutputDir(description="output directory")
@@ -11,6 +14,68 @@ class IO_Schema(ags.ArgSchema):
     projection_threshold = ags.fields.Int(default=0)
     normalize_proj_mat = ags.fields.Boolean(default=True)
 
+
+def de_layer(st):
+    CTX_STRUCTS = STRUCTURE_DESCENDANTS_ACRONYM['CTX']
+    sub_st = st.replace("ipsi_","").replace("contra","")
+    if sub_st in CTX_STRUCTS:
+            
+        for l in ["1","2/3","4","5","6a","6b"]:
+            st = st.replace(l,"")
+            
+        if "ENT" in st:
+            for l in ["2", "3","6"]:
+                st = st.replace(l,"")
+            
+        return st
+    else:
+        return st
+
+
+def roll_up_proj_mat(infile, outfile):
+    
+    df = pd.read_csv(infile, index_col=0)
+    df.index = df.index.map(os.path.basename)
+    
+    non_proj_cols = [f for f in df.columns if not any([i in f for i in ["ipsi","contra"]])]
+    new_df = df[non_proj_cols].copy()
+    
+    proj_cols = [f for f in df.columns if any([i in f for i in ["ipsi","contra"]])]
+    de_layer_dict = {p:de_layer(p) for p in proj_cols}
+    
+    parent_names = list(de_layer_dict.values())
+    unique_parent_names = np.unique(parent_names)
+    unique_parent_names = sorted(unique_parent_names, key=lambda x:parent_names.index(x))
+    
+    roll_up_records = {}
+    for low_res_struct in unique_parent_names:
+        children = [k for k,v in de_layer_dict.items() if v==low_res_struct ]
+        roll_up_records[low_res_struct] = children
+    
+    
+    
+    # for parent, child_list in roll_up_records.items():
+    #     new_df[parent] = df[child_list].sum(axis=1)
+    new_cols = {
+        parent: df[child_list].sum(axis=1)
+        for parent, child_list in roll_up_records.items()
+    }
+    new_cols_df = pd.DataFrame(new_cols)
+    new_df = pd.concat([new_df, new_cols_df], axis=1)
+    
+    # sanity check
+    for n_struct,old_list in roll_up_records.items():
+        sum_old = df[old_list].sum(axis=1)
+        sum_new = new_df[n_struct]
+        assert sum(sum_old==sum_new) == len(df)
+    
+    
+    
+    # print(outfile)
+    # print()
+    assert os.path.abspath(outfile) != os.path.abspath(infile)
+    new_df.to_csv(outfile)
+    
 
 def normalize_projection_columns_per_cell(input_df, projection_column_identifiers=['ipsi', 'contra']):
     """
@@ -54,6 +119,7 @@ def main(output_directory,
     # proj_df_mask = pd.DataFrame(branch_and_tip_projection_records).T.fillna(0)
 
     proj_df.to_csv(output_projection_csv)
+    roll_up_proj_mat(infile=output_projection_csv, outfile=output_projection_csv.replace(".csv",'_rollup.csv'))
     # proj_df_mask.to_csv(output_projection_csv_tip_branch_mask)
 
     if projection_threshold != 0:
@@ -79,6 +145,7 @@ def main(output_directory,
 
         proj_df = normalize_projection_columns_per_cell(proj_df)
         proj_df.to_csv(output_projection_csv)
+        roll_up_proj_mat(infile=output_projection_csv, outfile=output_projection_csv.replace(".csv",'_rollup.csv'))
 
         # proj_df_mask = normalize_projection_columns_per_cell(proj_df_mask)
         # proj_df_mask.to_csv(output_projection_csv_tip_branch_mask)
